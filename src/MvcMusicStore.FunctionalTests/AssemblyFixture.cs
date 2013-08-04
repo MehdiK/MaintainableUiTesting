d196ea4323263d33f6f0cb6b88415cced13571c0
MehdiK <me@mehdi-khalili.com>
2013-08-04 18:05:21 +0430

    Initial commit - the website is running and the brittle test is in

---
+++ src/MvcMusicStore.FunctionalTests/AssemblyFixture.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace MvcMusicStore.FunctionalTests
+{
+    public class Host
+    {
+        internal static readonly SeleniumApplication Instance = new SeleniumApplication();
+    }
+
+    [SetUpFixture]
+    public class AssemblyFixture
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Host.Instance.Run("MvcMusicStore", 12345);
+        }
+    }
+}
